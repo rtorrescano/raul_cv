f26d4677b9a8922630bb66e0c770673c5be36e44
rtorrescano <32579587+rtorrescano@users.noreply.github.com>
2021-04-28 14:26:30 -0500

    gh-pages modified menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,10 +13,10 @@ class Menu extends Component{
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="flex-sm-fill text-sm-center nav-lin"></Nav>    
                     <Nav>
-                        <Nav.Link><NavLink to="/">Inicio</NavLink></Nav.Link>
-                        <Nav.Link><NavLink to="/about" >Acerca de mi</NavLink></Nav.Link>
-                        <Nav.Link><NavLink to="/experience" >Experiencia</NavLink></Nav.Link>
-                        <Nav.Link><NavLink to="/contact" >Contacto</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/raul_cv">Inicio</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/raul_cv/about" >Acerca de mi</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/raul_cv/experience" >Experiencia</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/raul_cv/contact" >Contacto</NavLink></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
